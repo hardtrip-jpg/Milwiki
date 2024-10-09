01cdbdae743d5a4555bc429f486789bc4d31ca7d
hardtrip <user@hardtrip.ca>
2024-10-09 15:49:17 -0400

    buttons generate buttons

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
+function createButton() {
+    const button = document.createElement("button");
+    button.innerText = "Test";
+    button.classList.add("button-46");
 
-  
+    button.addEventListener("click", () => {
+        buttonClicked();
+    })
+
+    const button_holder = document.getElementById("button-holder");
+    button_holder.appendChild(button);
+};
+
+
+function buttonClicked() {
+
+    const random_gothru = (Math.floor(Math.random() * 4));
+    for (let i = 0; i < random_gothru; i++) {
+        createButton();
+    }
+}
