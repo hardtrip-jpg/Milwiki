a3f9331aa17ea19d3fdca10f7afc141ce2dc8be1
hardtrip <user@hardtrip.ca>
2024-10-09 18:24:19 -0400

    buttons now change article frame content

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+
+
 function createButton() {
     const button = document.createElement("button");
     button.innerText = "Test";
     button.classList.add("button-46");
 
     button.addEventListener("click", () => {
-        buttonClicked();
+        buttonClicked(Math.floor(Math.random() * 2) + 1);
     })
 
     const button_holder = document.getElementById("button-holder");
@@ -12,10 +14,26 @@ function createButton() {
 };
 
 
-function buttonClicked() {
+function buttonClicked(articleID) {
 
-    const random_gothru = (Math.floor(Math.random() * 4));
-    for (let i = 0; i < random_gothru; i++) {
+    const random_gothru = (Math.floor(Math.random() * 2) + 1);
+    for (let i = 0; i <= random_gothru; i++) {
         createButton();
+    };
+    console.log(articleID);
+    document.getElementById('article-holder').src = matchArticle(articleID);
+}
+
+
+
+
+function matchArticle(articleID) {
+    switch (articleID) {
+        case 1:
+            return "./html-files/test-article-1.html"
+        case 2:
+            return "./html-files/test-article-2.html"
+        default:
+            return "./html-files/test-article-1.html"
     }
 }
